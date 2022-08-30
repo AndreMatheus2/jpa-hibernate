2a8c9da81bf3c802bcd1388681773782c13cce58
andrematheus2 <am_arao@hotmail.com>
2022-08-30 10:05:44 -0300

    novas entidades cliente e pedido

---
+++ loja/src/main/java/br/com/andre/loja/modelo/Pedido.java
@@ -0,0 +1,62 @@
+package br.com.andre.loja.modelo;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pedidos")
+public class Pedido {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private BigDecimal valorTotal;
+	private LocalDate date = LocalDate.now();
+	@ManyToOne
+	private Cliente cliente;
+	
+	
+	
+	 public Pedido() {
+	}
+
+	public Pedido(Cliente cliente) {
+		super();
+		this.cliente = cliente;
+	}
+
+	public BigDecimal getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(BigDecimal valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	
+
+}
